093b5d8c7e153754cb9708b4a1602de89c537629
Fabian Meyer <FaMeyer@brita.net>
2020-12-01 09:54:08 +0100

    Did some restructuring and added comments

--- Day 01 - Expense Report/Program.fs
+++ Day 01 - Expense Report/Program.fs
@@ -1,47 +1,58 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System.IO
 
-open System.IO
-
-let rec findPair lookup elements =
-    match elements with
+// Find first matching pair using lookup map 
+let rec findPair year lookup input =
+    match input with
     | (x :: xs) ->
-        match Map.tryFind (2020 - x) lookup with
-        | Some _ -> Some((x, 2020 - x))
-        | None -> findPair (Map.add x true lookup) xs
+        match Map.tryFind (year - x) lookup with
+        | Some y -> Some((x, y))
+        | None -> findPair year (Map.add x x lookup) xs
     | [] -> None
 
-let rec mkPairs lst =
-    match lst with
-    | (x :: xs) -> List.map (fun y -> (x, y)) xs @ mkPairs xs
+
+// Iterate all permutation pairs 
+let rec permutations input =
+    match input with
+    | (x :: xs) -> List.map (fun y -> (x, y)) xs @ permutations xs
     | [] -> []
 
-[<EntryPoint>]
-let main argv =
-    let input =
-        File.ReadLines "input.txt"
-        |> Seq.toList
-        |> List.map int
-
-    input
-    |> findPair Map.empty
-    |> fun x ->
-        printfn "%A" x
-        x
-    |> Option.map (fun (x, y) -> x * y)
-    |> printfn "%A"
-
-    mkPairs input
-    |> List.filter (fun (x, y) -> x + y <= 2020)
+// Find first matching pair using combinations
+let findTriple year input =
+    permutations input
+    |> List.filter (fun (x, y) -> x + y <= year)
     |> List.collect (fun (x, y) ->
         input
         |> List.filter (fun z -> z <> x)
         |> List.filter (fun z -> z <> y)
         |> List.map (fun z -> (x, y, z)))
-    |> List.filter (fun (x, y, z) -> x + y + z = 2020)
-    |> List.tryHead
-    |> fun x ->
-        printfn "%A" x
-        x
+    |> List.tryFind (fun (x, y, z) -> x + y + z = year)
+
+// Print intermediate result and return it
+let printEffect x =
+    printfn "%A" x
+    x
+
+[<EntryPoint>]
+let main argv =
+    let year = 2020
+
+    // Read input
+    let input =
+        File.ReadLines "input.txt"
+        |> Seq.toList
+        |> List.map int
+
+    // Part 1
+    input
+    |> findPair year Map.empty
+    |> printEffect
+    |> Option.map (fun (x, y) -> x * y)
+    |> printfn "%A"
+
+    // Part 2
+    input
+    |> findTriple year
+    |> printEffect
     |> Option.map (fun (x, y, z) -> x * y * z)
     |> printfn "%A"
 
