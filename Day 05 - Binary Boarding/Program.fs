05f096a45641880e09f33c54aabc2b2a2d50a3bd
Fabian Meyer <FaMeyer@brita.net>
2020-12-06 18:53:05 +0100

    Day 5

---
+++ Day 05 - Binary Boarding/Program.fs
@@ -0,0 +1,30 @@
+﻿open System.IO
+
+let toSeatId (code: string) =
+    Seq.fold (fun seatId c ->
+        match c with
+        | 'F' -> seatId <<< 1
+        | 'B' -> seatId <<< 1 ||| 1
+        | 'L' -> seatId <<< 1
+        | 'R' -> seatId <<< 1 ||| 1
+        | _ -> failwith "Invalid") 0 code
+
+[<EntryPoint>]
+let main argv =
+    let input =
+        File.ReadAllLines "input.txt" |> Seq.map toSeatId
+
+    let takenSeats = input |> Seq.sort |> Seq.toList
+
+    // Part 1
+    let highestSeat = List.max takenSeats
+    printfn "%d" (highestSeat)
+
+    // Part 2
+    List.pairwise takenSeats
+    |> List.filter (fun (l, r) -> r - l = 2)
+    |> List.map (fun (l, _) -> l + 1)
+    |> List.head
+    |> printfn "%d"
+
+    0
